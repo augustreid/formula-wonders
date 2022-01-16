a363d96fa8607d61dccee0f7346461b26b7f56b9
August Reid <c.august.reid@gmail.com>
2022-01-16 11:59:01 -0700

    Debug using DnD API

--- src/Components/RaceResults.js
+++ src/Components/RaceResults.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "../SCSS/RaceResults.scss";
+import useFetch from "../hooks/useFetch";
 
 const RaceResults = () => {
+    const {data, isLoading, error} = useFetch("https://www.dnd5eapi.co/api/spells/acid-arrow/")
+    console.log(data)
+    const higherLevel = data["higher_level"][0];
+    // // const wizard = data.classes[0];
+
     return (
-        <h3>Race Results</h3>
+        <div>
+            <h3>Race Results</h3>
+            <p>{higherLevel}</p>
+        </div>
+        
     )
 }
 
