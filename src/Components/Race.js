20e76b1fc0abfc2f59b852c8e7fb79d27095b672
August Reid <c.august.reid@gmail.com>
2022-01-18 16:03:51 -0700

    Add propTypes to Drivers

--- src/Components/Race.js
+++ src/Components/Race.js
@@ -3,33 +3,32 @@ import "../SCSS/Schedule.scss";
 
 
 const Race = ({ name, circuit, date, time }) => {
-    const raceDate = new Date(date);
 
-    return (
-        <div>
-            <h3>{name}</h3>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Circuit:</th>
-                        <td>{circuit.circuitName}</td>
-                    </tr>
-                    <tr>
-                        <th>Location:</th>
-                        <td>{`${circuit.Location.locality}, ${circuit.Location.country}`}</td>
-                    </tr>
-                    <tr>
-                        <th>Date:</th>
-                        <td>{date}</td>
-                    </tr>
-                    <tr>
-                        <th>Time:</th>
-                        <td>{time}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    )
+  return (
+    <div>
+      <h3>{name}</h3>
+      <table>
+        <tbody>
+          <tr>
+            <th>Circuit:</th>
+            <td>{circuit.circuitName}</td>
+          </tr>
+          <tr>
+            <th>Location:</th>
+            <td>{`${circuit.Location.locality}, ${circuit.Location.country}`}</td>
+          </tr>
+          <tr>
+            <th>Date:</th>
+            <td>{date}</td>
+          </tr>
+          <tr>
+            <th>Time:</th>
+            <td>{time}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default Race; 
