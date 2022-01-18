b529cec1b20351ffa6e4005066a47c6ccec08648
August Reid <c.august.reid@gmail.com>
2022-01-17 19:41:18 -0700

    Render driver name

--- src/Components/SingleDriver.js
+++ src/Components/SingleDriver.js
@@ -4,10 +4,21 @@ import useFetch from "../hooks/useFetch";
 
 const SingleDriver = () => {
     const driverId = useParams().id;
-    const {data: driverBio, isLoading, error} = useFetch(`http://ergast.com/api/f1/drivers/${driverId}.json`)
-    
+    const {data: driverInfo, isLoading, error} = useFetch(`http://ergast.com/api/f1/drivers/${driverId}.json`)
+    let driver;
+
+    if (!isLoading) {
+        driver = driverInfo.DriverTable.Drivers[0];
+    }    
+
     return (
-        <p>testing</p>
+        <div>
+       {!isLoading && 
+       <section>
+            <h3>{`${driver.givenName} ${driver.familyName}`}</h3>
+            
+        </section>}
+        </div>
     )
 }
 
