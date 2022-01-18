874b831791369c21bfd47434867b04bb5b719ea7
August Reid <c.august.reid@gmail.com>
2022-01-17 21:38:20 -0700

    Fetch and render table of wins stats

--- src/Components/Wins.js
+++ src/Components/Wins.js
@@ -1,10 +1,44 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
+import Loading from "./Loading";
 
 
 const Wins = ({driverId}) => {
+    const {data: champions, isLoading, error} = useFetch(`https://ergast.com/api/f1/drivers/${driverId}/driverStandings/1.json`);
+    const {data: wins} = useFetch(`https://ergast.com/api/f1/drivers/${driverId}/results/1.json?limit=300`);
+    const {data: seconds} = useFetch(`https://ergast.com/api/f1/drivers/${driverId}/results/2.json?limit=300`);
+    const {data: thirds} = useFetch(`https://ergast.com/api/f1/drivers/${driverId}/results/3.json?limit=300`);
+    let championshipWins;
+    let raceWins;
+    let podiums;
+
+    if (!isLoading) {
+        championshipWins = champions.total;
+        raceWins = wins.total;
+        podiums = parseInt(wins.total) + parseInt(seconds.total) + parseInt(thirds.total);
+    }
 
     return (
-        <p>testing</p>
+        <div>
+            {error && <p>{error}</p>}
+            {isLoading ? <Loading /> :
+            <table>
+                <tbody>
+                    <tr>
+                        <th>World Championship Wins:</th>
+                        <td>{championshipWins}</td>
+                    </tr>
+                    <tr>
+                        <th>Race Wins:</th>
+                        <td>{raceWins}</td>
+                    </tr>
+                    <tr>
+                        <th>Race Podiums:</th>
+                        <td>{`${podiums}`}</td>
+                    </tr>
+                </tbody>
+            </table>}
+        </div>
     )
 
 }
